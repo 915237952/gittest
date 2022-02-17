105617fddac3f5fced59d68a6df4d56156d7df33
915237952 <915237952@qq.com>
2022-02-17 18:31:30 +0800

    新建asd类

---
+++ src/main/java/com/lanou/test/asd.java
@@ -0,0 +1,11 @@
+package com.lanou.test;
+
+/*
+ * Created by MM on 2022/2/17
+ */
+
+public class asd {
+    public static void main(String[] args) {
+        System.out.println("aaaaa");
+    }
+}
