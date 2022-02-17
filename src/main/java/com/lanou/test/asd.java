b3eff7130d8e93b1ba497474ecd6a0b5a96e1f6a
915237952 <915237952@qq.com>
2022-02-17 19:09:04 +0800

    删除代码

--- src/main/java/com/lanou/test/asd.java
+++ src/main/java/com/lanou/test/asd.java
@@ -6,8 +6,6 @@ package com.lanou.test;
 
 public class asd {
     public static void main(String[] args) {
-        System.out.println("aaaaa");
-        System.out.println("asdasd");
-        System.out.println("asdasasdasd");
+        
     }
 }
